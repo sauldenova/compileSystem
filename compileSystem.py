392429b2c910044ec3689f88dfd00621da8dd786
lkt345 <lkt345@gmail.com>
2012-06-18 12:54:58 -0500

    corrections

--- compileSystem.py
+++ compileSystem.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
-import fnmatch, os, subprocess, sys, re
+#TODO CONTROL-C BEHAVIOR
+
+import fnmatch
+import os
+import subprocess
+import signal
+import sys
+import re
 from optparse import OptionGroup, OptionParser
 
 class bcolors:
@@ -26,6 +33,9 @@ def stringSplitByNumbers(x):
     l = r.split(x)
     return [int(y) if y.isdigit() else y for y in l]
 		
+#Check if a string has search values
+def specialMatch(strg, search=re.compile(r'[^A-Za-z0-9.]').search): 
+	return bool(search(strg))
 
 def TrueXor(*args):
 	return sum(args)<=1
@@ -69,7 +79,7 @@ def compileSource(sourceFile):
 	
 	#Open the make configuration file
 	makeFile=open('.makeFile', 'w')
-	makeFile.write('all:\n\t' + compiler + ' -g ' + sourceFile + ' -o ' + fileName + '\n')
+	makeFile.write('all:\n\t' + compiler + ' -g ' + sourceFile + ' -o ' + fileName + ' -O2\n')
 	makeFile.close()
 	
 	#Call make
@@ -102,51 +112,84 @@ def compileSource(sourceFile):
 	return fileName
 
 		
-def evaluate(sourceFile, currentDirectory, maximumTime, verbose):
+def evaluate(sourceFile, currentDirectory, maximumTime, verbose, ioiMode):
+	'''Evaluate the source file with the .in cases found in dir'''
 	total=0
 	testCases=0
 	executable, extension = os.path.splitext(sourceFile)
-	for IN in sorted(locate("*.in*", currentDirectory), key = stringSplitByNumbers) :
+	for IN in sorted(locate("*.in*", currentDirectory), key = stringSplitByNumbers) : #For each file that has .in
 		OUT=IN.replace(".in", ".out")
-		try:
+		try: #If .out exists
 			open(OUT, "r");
 		except IOError as e:
 			continue
 		
-		os.system("ulimit -t " + str(maximumTime) + "; /usr/bin/time -o time ./" + executable + " < " + IN + " > ./temporal.out")
-		os.system("tr -d ' \t\n\r\f' < ./temporal.out > ./temp1.out")
-		os.system("tr -d ' \t\n\r\f' < " + OUT + " > ./temp2.out")
+		segFault=False
+		#Execute the process
+		#ulimit kills the process if it uses more than the given time
+		#Uses time for taking the time of the process
+		#The output is stored in temporal.out file
+		varStack=""
+		if ioiMode:
+			varStack="ulimit -s unlimited; "
+		if not specialMatch(executable):
+			executable="./"+executable;
+		globalLimits=varStack + "ulimit -t " + str(maximumTime) + "; " 
+		try:
+			os.system(globalLimits + "time -o /tmp/cstime " + executable + " < " + IN + " > /tmp/cs.out 2> /tmp/cserror")
+		except MemoryError as e:
+			segFault=True
+		#For comparation of programs that have multiple solutions,
+		#this script removes all spaces from files and turns them into strings
+		os.system("tr -d ' \t\n\r\f' < /tmp/cs.out > /tmp/cs1.out") 
+		os.system("tr -d ' \t\n\r\f' < " + OUT + " > /tmp/cs2.out")
 		
+		#Obtain the case number from the IN name 
 		caseNumber=IN.replace(os.getcwd(), "")
-		caseNumber=caseNumber.replace(currentDirectory, "")
-		caseNumber=re.sub("\D", "", caseNumber)
-		temp1=open('./temp1.out', 'r')
-		temp2=open('./temp2.out', 'r')
-		timeFile=open('./time', 'r')
+		caseNumber=caseNumber.replace(os.path.dirname(IN), "")
+		caseNumber=caseNumber.replace(os.path.basename(executable), "")
+		caseNumber=caseNumber.replace(".in", "")
+		caseNumber=caseNumber.replace(".", "")
+		caseNumber=caseNumber.replace("/", "")
+
+		#Open the file of strings
+		temp1=open('/tmp/cs1.out', 'r')
+		temp2=open('/tmp/cs2.out', 'r')
+		errorFile=open('/tmp/cserror', 'r')
+
+		str1=temp1.read()
+		str2=temp2.read()
+		strError=errorFile.read()
+
+		#Find the time used by the program
+		timeFile=open('/tmp/cstime', 'r')
 		time=timeFile.readline()[0:4]
 		if not isNumber(time):
 			time=timeFile.readline()[0:4]
 		#time=time.replace(".", "")
-		str1=temp1.read()
-		str2=temp2.read()
 		
-		if float(time)>maximumTime:
+		if (float(time)+0.03)>=float(maximumTime) and not str1 in str2: #If time was exceded
 			if verbose:
 				sys.stdout.write(bcolors.FAIL + "CASE " + caseNumber + ":TLE\t\t")
 				sys.stdout.write(bcolors.OKBLUE + "TIME ELAPSED: " + time + "\n" + bcolors.ENDC)
-		elif str1 in str2:
+		elif strError != "" or str1 == "" or segFault:
 			if verbose:
-				sys.stdout.write(bcolors.OKGREEN + "CASE " + caseNumber + ":CO\t\t")
+				sys.stdout.write(bcolors.FAIL + "CASE " + caseNumber + ":RTE\t\t")
+				sys.stdout.write(bcolors.OKBLUE + "TIME ELAPSED: " + time + "\n" + bcolors.ENDC)
+		elif str1 in str2 or str1 == "CORRECT": #If output file string is in the case string
+			if verbose:
+				sys.stdout.write(bcolors.OKGREEN + "CASE " + caseNumber + ":OK\t\t")
 				sys.stdout.write(bcolors.OKBLUE + "TIME ELAPSED: " + time + "\n" + bcolors.ENDC)
 			total+=1
-		else:
+		else: #If not case is wrong
 			if verbose:
 				sys.stdout.write(bcolors.FAIL + "CASE " + caseNumber + ":WA\t\t")
 				sys.stdout.write(bcolors.OKBLUE + "TIME ELAPSED: " + time + "\n" + bcolors.ENDC)
 		testCases+=1
 		
-		os.system("rm time temporal.out temp1.out temp2.out")
-			
+		#Remove temporary files
+		os.system("rm /tmp/cstime /tmp/cstemporal.out /tmp/cstemp1.out /tmp/cstemp2.out /tmp/cstempError")
+	
 	if testCases>0:
 		if verbose:
 			sys.stdout.write(bcolors.HEADER + "TOTAL: " + str(total*100/testCases) + "\n" + bcolors.ENDC)
@@ -155,10 +198,18 @@ def evaluate(sourceFile, currentDirectory, maximumTime, verbose):
 	else:
 		sys.stdout.write(bcolors.FAIL + "ERROR COULD NOT FIND CASES FOR " + sourceFile + "\n" + bcolors.ENDC)
 
+def signalHandler(signal, frame):
+	'''Kill subprocess for testing'''
+	sys.stdout.write(bcolors.FAIL + "Subprocess received signal SIGINT stopping testing\n" + bcolors.ENDC)
+	os.kill(testingProcess.pid, signal.SIGTERM)
 
-#
-#MAIN START
-#
+def preexecFunction():
+	'''Overwrite the SIGINT signal handler for signalHandler'''
+	signal.signal(signal.SIGINT, signalHandler);
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#-=-=-=-=-=-=-=-=-=-=-=-=MAIN START-=-=-=-=-=-=-=-=-=-=-=-=-=
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 #PARSER
 parser=OptionParser(usage="%prog [OPTION]... [FILE]...", 
@@ -179,15 +230,15 @@ parser.add_option("-e", "--evaluate",
 				  action="store_true", dest="evaluate", default=False,
 				  help="Evaluates the code with the test cases found in DIR")
 
-#CORE UTILS
-coreUtils=OptionGroup(parser, "Core utilities")
-coreUtils.add_option("--output-file",
-				  	 action="store_true", dest="stdoutRedirection", default=False,
-				  	 help="Pipes all std output STDOUT to csout.log and STDERR to cserr.log")
-coreUtils.add_option("-c", "--copy",
-				  	 action="store", type="string", dest="copyFile", default="",
-				  	 help="Copies the contents of file SOURCE to the X11 clipboard", metavar="SOURCE")
-parser.add_option_group(coreUtils)
+#MISCELLANEOUS UTILS
+miscellaneousUtils=OptionGroup(parser, "Miscellaneous utilities")
+miscellaneousUtils.add_option("--output-file",
+				  action="store_true", dest="stdoutRedirection", default=False,
+				  help="Pipes all std output STDOUT to csout.log and STDERR to cserr.log")
+miscellaneousUtils.add_option("-c", "--copy",
+				  action="store", type="string", dest="copyFile", default="",
+				  help="Copies the contents of file SOURCE to the X11 clipboard", metavar="SOURCE")
+parser.add_option_group(miscellaneousUtils)
 
 #TESTING UTILS
 testingUtils=OptionGroup(parser, "Testing utilities")				  
@@ -199,15 +250,20 @@ parser.add_option_group(testingUtils)
 #EVALUATION UTILS
 evaluationUtils=OptionGroup(parser, "Evaluation utilies")
 evaluationUtils.add_option("-w", "--directory",
-				  		   action="store", type="string", dest="workingDirectory", default=".",
-				  		   help="Changes the working directory DIR. By default DIR is \'.\'", metavar="DIR")
+				  action="store", type="string", dest="workingDirectory", default=".",
+				  help="Changes the working directory DIR. By default DIR is \'.\'", metavar="DIR")
 evaluationUtils.add_option("-t", "--time",
-				  		   action="store", type="string", dest="evaluationTime", default=1,
-				  		   help="Defines TIME during the program can be evaluated. Is 1 by default", metavar="TIME")
+				  action="store", type="string", dest="evaluationTime", default=1,
+				  help="Defines TIME during the program can be evaluated. Is 1 by default", metavar="TIME")
 evaluationUtils.add_option("--no-verbose",
-				  		   action="store_false", dest="verbose", default=True,
-				  		   help="Disables detailed output for evaluation. If not enables only prints total")
+				  action="store_false", dest="verbose", default=True,
+				  help="Disables detailed output for evaluation. If not enables only prints total")
+evaluationUtils.add_option("--new-ioi-mode",
+				  action="store_true", dest="ioiMode", default=False,
+				  help="Enables new IOI rules mode for evaluation of cases and unlimited stack size")
 parser.add_option_group(evaluationUtils)
+
+#optcomplete.autocomplete(parser)
 
 (options, args)=parser.parse_args()
 
@@ -220,13 +276,16 @@ if not TrueXor(options.compile, options.debug, options.test, options.evaluate):
 	sys.stderr.write(bcolors.FAIL + "MORE THAN ONE CORE OPTION WAS USED\nKILLING PROCESS\n" + bcolors.ENDC);
 	sys.exit()
 
-#STDOUT Redirection
+#STDOUT REDIRECTION
 if options.stdoutRedirection:
 	sys.stdout=open("csout.log", "w")
 	sys.stderr=open("cserr.log", "w")
 
-#EXECUTE OPTIONS
+#CLIPBOARD COPY
+if options.copyFile != "":
+	os.system("cat " + options.copyFile + " | xclip -selection c")	
 
+#EXECUTE OPTIONS
 for file in args:
 	try:
 		open(file, "r")
@@ -243,10 +302,10 @@ for file in args:
 		executable=compileSource(file)
 		if executable != "": 
 			for i in range(1, options.testingTimes+1): #Run the program testingTimes
-				sys.stdout.write('Testing ' + executable + ': ' + i + 'times\n')
-				subprocess.call([executable])
+				sys.stdout.write(bcolors.DEBUG + 'Testing ' + executable + ': ' + str(options.testingTimes-i+1) + ' times\n' + bcolors.ENDC)
+				subprocess.call(['./' + executable]); #TODO change Ctrl-C Behavior
 	elif options.evaluate: #Evaluate
 		executable=compileSource(file)
 		if executable != "":
-			evaluate(file, options.workingDirectory, options.evaluationTime, options.verbose)
+			evaluate(file, options.workingDirectory, options.evaluationTime, options.verbose, options.ioiMode)
 
