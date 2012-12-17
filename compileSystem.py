912e9827c240fac81356b23553f154125fa92c2c
Saul de Nova <sauldenova@sdenova-NV53.(none)>
2012-12-16 20:44:57 -0600

    Working version and removed false README

--- compileSystem.py
+++ compileSystem.py
@@ -20,7 +20,7 @@ POSSIBLEFILEOUTEND = ['.out', '.sol']
 
 import os
 import re
-import resource
+import resource as res
 import subprocess
 import signal
 import time
@@ -30,6 +30,8 @@ from optparse import OptionGroup, OptionParser
 
 # Class for the more than one option used error
 class MoreOptionsError(ValueError): pass
+# Class for invalid parameter error
+class ParamError(ValueError): pass
 
 # ASCII color codes for printing in terminal
 class bcolors:
@@ -49,22 +51,98 @@ class bcolors:
 		self.FAIL = ''
 		self.ENDC = ''
 
-# Possible options declaration for interactive prompt
-class PossibleOptions():
-	pass
-    
 # Object declaration for bcolors
 bcolorsObject = bcolors()
 
+def exitFunction(args):
+	raise EOFError
+
+def helpFunction(args):
+	print(helpMessage)
+
+def validateOption(args):
+	'''Check if the parameter can be of the correct type'''
+	value, param = args[:2]
+	try:
+		if value in defaultOptionsDict:
+			valueType = type(defaultOptionsDict[value])
+			if valueType == bool:
+				if param in ['False', 'false', 'f', 'F']:
+					param = False
+				elif param in ['True', 'true', 't', 'T']:
+					param = True
+				elif param == '':
+					param = not(defaultOptionsDict[value])
+				else:
+					raise ValueError
+			elif valueType == str:
+				try:
+					open(param, 'r')
+				except IOError as e:
+					if e.errno == 2:
+						print('File or directory does not exist')
+						raise ValueError
+			else:
+				param = type(defaultOptionsDict[value])(param)
+		else:
+			raise MoreOptionsError
+	except ValueError:
+		raise ParamError
+	except MoreOptionsError:
+		raise MoreOptionsError
+	else:
+		defaultOptionsDict[value] = param
+		print('Set %s to %s' % (value, param))
+
+def evalFunction():
+	"""Checks for the evaluation option and sets the correct parameters"""
+	pass
+
+# Possible options dictionary
+optionsDict = {'exit':exitFunction,
+			   'quit':exitFunction,
+			   'help':helpFunction,
+			   'output-file':validateOption,
+			   'optimize':validateOption,
+			   'compile':validateOption,
+			   'colors-prompt':validateOption,
+			   'runs':validateOption,
+			   'directory':validateOption,
+			   'time':validateOption,
+			   'memory':validateOption,
+			   'verbose':validateOption,
+			   'output-files-comp':validateOption,
+			   'multiple-solutions':validateOption,
+			   'alternate-values':validateOption,
+			   'new-ioi-mode':validateOption,
+			   'compile':evalFunction,
+			   'evaluate':evalFunction,
+			   'test':evalFunction,
+			   'debug':evalFunction}
+
+# Options for evaluation dictionary
+defaultOptionsDict = {'output-file':False,
+					  'optimize':True,
+					  'compile':True,
+					  'colors-prompt':True,
+					  'runs':10,
+					  'directory':'.',
+					  'time':1,
+					  'memory':64,
+					  'verbose':True,
+					  'output-files-comp':True,
+					  'multiple-solutions':True,
+					  'alternate-values':'',
+					  'new-ioi-mode':False}
+
 def stringSplitByNumbers(x):
-    r = re.compile('(\d+)')
-    l = r.split(x)
-    return [int(y) if y.isdigit() else y for y in l]
+	'''Correct ordering for file numbers'''
+	r = re.compile('(\d+)')
+	l = r.split(x)
+	return [int(y) if y.isdigit() else y for y in l]
 		
 def routeSpecified(strg, search=re.compile(r'[^A-Za-z0-9.]').search): 
-	'''
-	Check if a string has only alphanumeric values, used for finding if a program has a path specified
-	'''
+	'''Check if a string has only alphanumeric values, used for finding if a program has a path specified'''
 	return bool(search(strg))
 
 def trueXor(*args):
@@ -84,14 +162,12 @@ def killProcess(sign, frame):
 	os.exit(0)
 
 def compileSource(sourceFile, verbose=True, optimized=True):
-	'''
-	Compile the source file provided by the parser
+	'''Compile the source file provided by the parser
 	sourceFile is a string to the path of the file to be compiled
 	verbose specifies if the code is to print the warning and error messages
 	optimized adds the -O2 flag to the compiler
 	returns the name of the executable file. It is compiled in the current directory(os.currdir)
-	if an error was encountered, returns ''
-	'''
+	if an error was encountered, returns '' '''
 	# Check if source exists
 	try:
 		open(sourceFile, 'r')
@@ -110,12 +186,9 @@ def compileSource(sourceFile, verbose=True, optimized=True):
 		return ''
 	
 	# Open the make configuration file
-	optimization = ''
-	if optimized:
-		optimization = '-O2'
-	makeFile = open('.makeFile', 'w')
-	print('all:\n\t %s -g %s -o %s %s' % (compiler, sourceFile, fileName, optimization), file = makeFile)
-	makeFile.close()
+	optimization = ('-O2' if optimized else '')
+	with open('.makeFile', 'w') as makeFile:
+		print('all:\n\t %s -g %s -o %s %s' % (compiler, sourceFile, fileName, optimization), file = makeFile)
 	
 	# Call make
 	with open('.compile.log', 'w') as compileLog:
@@ -151,26 +224,24 @@ def compileSource(sourceFile, verbose=True, optimized=True):
 
 def evaluate(sourceFile, currentDirectory, maximumTime=1, verbose=False, 
 			 ioiMode=False, memory=64, noOuts=False, multipleSolutions=False, alternateValues=''):
-	'''
-	Evaluate the source file with the .in cases found in currentDirectory
+	'''Evaluate the source file with the .in cases found in currentDirectory
 	By default the maximum time is the time for the program to be evaluated
 	Verbose mode is true by default, if set to false, the program will only print errors and the end results
 	ioiMode allows an infinite stack only to be limited by memory
 	memory is the maximum size for the virtual memory
 	noOuts only checks the program for Runtime Errors and Time Limit Exceeded not evaluating the result for each case
 	multipleSolutions is similar to noOuts, only it check for a substring in the output file
-	alternateValues is a specialized mode if some cases are worth more than others
-	'''
+	alternateValues is a specialized mode if some cases are worth more than others'''
 	maxMemoryInBytes = int(memory*1024*1024)
 	maxTimeInSeconds = int(maximumTime)
 
 	def processLimit():
 		try:
-			resource.setrlimit(resource.RLIMIT_NPROC, (1, 1))
+			res.setrlimit(res.RLIMIT_NPROC, (1, 1))
 			if ioiMode:
-				resource.setrlimit(resource.RLIMIT_STACK, (RLIM_INFINITY, RLIM_INFINITY))
-			resource.setrlimit(resource.RLIMIT_CPU, (maxTimeInSeconds, maxTimeInSeconds))
-			resource.setrlimit(resource.RLIMIT_AS, (maxMemoryInBytes, maxMemoryInBytes))
+				res.setrlimit(res.RLIMIT_STACK, (RLIM_INFINITY, RLIM_INFINITY))
+			res.setrlimit(res.RLIMIT_CPU, (maxTimeInSeconds, maxTimeInSeconds))
+			res.setrlimit(res.RLIMIT_AS, (maxMemoryInBytes, maxMemoryInBytes))
 		except ValueError as e:
 			stderr.write(bcolorsObject.FAIL + 'Limit specified is invalid\n%s' % e + bcolorsObject.ENDC)
 			exit(-1)
@@ -199,21 +270,23 @@ def evaluate(sourceFile, currentDirectory, maximumTime=1, verbose=False,
 	for fileInAddr in sorted(locate('*.in*', currentDirectory), key=stringSplitByNumbers):
 		#Search if file exists
 		try:
-			fileIn = open(fileInAddr, 'r')
+			open(fileInAddr, 'r')
 		except IOError:
 			print('Failed to open file %s' % (fileInAddr), file = cslog)
 			continue
 
 		#Check fileOutAddr file exists
+		strOutput=''
 		if not noOuts:
 			for posibleEnding in POSSIBLEFILEOUTEND:
 				fileOutAddr = fileInAddr.replace('.in', posibleEnding)
 				try:
-					fileOut = open(fileOutAddr, 'r')
+					# Converts the output of the program to a string
+					with open(fileOutAddr, 'r') as fileOut:
+						strOutput = fileOut.read()
+					break
 				except IOError:
 					continue
-				else:
-					break
 			else:
 				print('Failed to open file %s' % (fileOutAddr), file = cslog)
 
@@ -234,22 +307,18 @@ def evaluate(sourceFile, currentDirectory, maximumTime=1, verbose=False,
 				print(bcolorsObject.FAIL + 'Invalid value from table\nTERMINATING' + bcolorsObject.ENDC, file = stderr)
 				break
 		
-		# fileOut opens a temporary file in the current directory for the stdout to be redirected
-		fileTemporaryOut = open('cs.out', 'w')
+		# Specify route for the executable
 		if not routeSpecified(executable):
 			executable='./'+executable;
 		
-		# Start the program evaluation. timeStart and timeUsed are used to determine how much the program took
-		timeStart = time.time()
-		evaluationSubprocess = subprocess.Popen(executable, stdin=fileIn, stdout=fileTemporaryOut, preexec_fn=processLimit)
-		evaluationSubprocess.wait()
-		evaluationReturnCode = evaluationSubprocess.returncode
-		timeUsed = time.time() - timeStart
-
-		# Now the .in file is no longer needed
-		fileIn.close()
-		# Also close the temporary out file so that it can be read
-		fileTemporaryOut.close()
+		# fileOut opens a temporary file in the current directory for the stdout to be redirected
+		with open(fileInAddr, 'r') as fileIn, open('cs.out', 'w') as fileTemporaryOut:
+			# Start the program evaluation. timeStart and timeUsed are used to determine how much the program took
+			timeStart = time.time()
+			evaluationSubprocess = subprocess.Popen(executable, stdin=fileIn, stdout=fileTemporaryOut, preexec_fn=processLimit)
+			evaluationSubprocess.wait()
+			evaluationReturnCode = evaluationSubprocess.returncode
+			timeUsed = time.time() - timeStart
 
 		# Check if in the execution an error was found.
 		caseStatus = ''
@@ -274,20 +343,13 @@ def evaluate(sourceFile, currentDirectory, maximumTime=1, verbose=False,
 					  bcolorsObject.OKBLUE + 'TIME ELAPSED: %.02f' % (float(timeUsed)) + bcolorsObject.ENDC)
 			continue
 
-		# Converts the output of the program to a string
-		strOutput=''
-		if not noOuts:
-			strOutput = fileOut.read()
-			fileOut.close()
-
 		# Opens the output file from the program to 
 		try:
-			fileTemporaryOut = open('cs.out', 'r')
+			with open('cs.out', 'r') as fileTemporaryOut:
+				strOwnProgram = fileTemporaryOut.read()
 		except IOError:
 			print('Error opening cs.out', file = cslog)
 			continue
-		strOwnProgram = fileTemporaryOut.read()
-		fileTemporaryOut.close()
 
 		# Use regex sub to remove all whitespace from the string
 		strOwnProgram = re.sub(r'\s', '', strOwnProgram)
@@ -306,7 +368,7 @@ def evaluate(sourceFile, currentDirectory, maximumTime=1, verbose=False,
 
 		# If verbose mode
 		if verbose:
-			print(colorOut + 'CASE %s:%s\t\t' % (caseNumber, caseStatus) +
+			print(colorOut + 'CASE %i:%s\t\t' % (int(caseNumber), caseStatus) +
 				  bcolorsObject.OKBLUE + 'TIME ELAPSED: %.2f' % (float(timeUsed)) + bcolorsObject.ENDC)
 		
 	if testCases > 0 or alternateValues:
@@ -321,10 +383,8 @@ def evaluate(sourceFile, currentDirectory, maximumTime=1, verbose=False,
 		print(bcolorsObject.FAIL + 'Error: Could not find test cases for %s' % (sourceFile) + bcolorsObject.ENDC)
 
 def parseExpressions():
-	'''
-	Function that implements the parsing of expressions with python module optparse
-	Returns the list of the parsed options and the remaining args of the expression
-	'''
+	'''Function that implements the parsing of expressions with python module optparse
+	Returns the list of the parsed options and the remaining args of the expression'''
 	# Parser object declaration
 	parser = OptionParser(usage = '%prog [OPTION]... [FILE]...', 
 						  version = '%prog ' + str(VERSION), 
@@ -405,26 +465,63 @@ def interactCommandLine():
 	'''Interface for interacting with the user
 	   Returns a (dict with the options, list of the files to process) '''
 	welcomeMessage = 'Compile system %s\nCompile, evaluate and debug C/C++ programs\nType "help" for the available options' % VERSION
+	print(welcomeMessage)
+	while True:
+		try:
+			args = input('>>> ').split()
+			option = args[0]
+			if option in optionsDict:
+				optionsDict[option](args)
+			else:
+				print('Invalid option, please see the help screen')
+		except ParamError:
+			print('Invalid parameter, please see the help screen')
+
+def checkEvalType(options, *args):
+	"""Function that receives the options from the prompt and a list of executables and executes the correct function"""
+
+	(args,) = args
+	# Execute options
+	for sourceFile in args:
+		#Check if sourceFile exists
+		try:
+			open(sourceFile, 'r')
+		except IOError as e:
+			print(e);
+			print(bcolorsObject.FAIL + 'Error: File %s does not exist' % (str(sourceFile)) + bcolorsObject.ENDC, file = stderr)
+			continue
+		
+		#Compiling options
+		if options.compile or not options.noCompile:
+			executable = compileSource(sourceFile, options.verbose, options.optimize)
+		
+		if executable == '':
+			continue
+
+		if options.debug: #Debug
+			subprocess.call(['echo', '-ne', bcolorsObject.DEBUG])
+			subprocess.call(['gdb', '-q', executable])
+			subprocess.call(['echo', '-ne', bcolorsObject.ENDC])
+		elif options.test: #Test
+			for i in range(options.testingTimes): #Run the program testingTimes
+				print(bcolorsObject.DEBUG + 'Testing %s: %s times' % (str(executable), str(options.testingTimes - i)) + bcolorsObject.ENDC)
+				subprocess.call(['./' + executable]); 
+		elif options.evaluate: #Evaluate
+			evaluate(sourceFile, options.workingDirectory, options.evaluationTime, options.verbose, 
+					 options.ioiMode, options.totalMemory, options.noOuts, options.multipleSolutions, options.alternateValues)
+
+if __name__ == '__main__' :
+	'''Main entrance to the program if called as a script. Overrides SIGINT behavior, parses the input and
+	decides the correct options'''
+	#TODO Check if terminal has color support
+	
+	# Override SIGINT Ctrl^C behavior
+	signal.signal(signal.SIGINT, killProcess); 
+	
 	path, file = os.path.split(os.path.realpath(__file__))
 	with open(path + '/helpText.txt') as helpText:
 		helpMessage = helpText.read()
-	print(welcomeMessage)
-	while True:
-		option = input('>>> ')
-		if option == 'help':
-			print(helpMessage)
-		elif option in ['exit', 'quit']:
-			raise EOFError
-		else:
-			print(option)
 
-if __name__ == '__main__' :
-	'''
-	Main entrance to the program if called as a script. Overrides SIGINT behavior, parses the input and
-	decides the correct options
-	'''
-	#TODO Check if terminal has color support
-	
 	if len(argv) < 2:
 		try:
 			(options, args) = interactCommandLine()
@@ -434,9 +531,6 @@ if __name__ == '__main__' :
 	else:
 		# Expresion parsing
 		(options, args) = parseExpressions()
-
-	# Override SIGINT Ctrl^C behavior
-	signal.signal(signal.SIGINT, killProcess); 
 
 	# Stdout redirection
 	if options.stdoutRedirection:
@@ -460,32 +554,5 @@ if __name__ == '__main__' :
 			print(bcolorsObject.FAIL + 'Error: Could not find file for copying' + bcolorsObject.ENDC, file = stderr)
 		else:
 			os.system('cat ' + options.copyFile + ' | xclip -selection c')	
-
-	# Execute options
-	for sourceFile in args:
-		#Check if sourceFile exists
-		try:
-			open(sourceFile, 'r')
-		except IOError as e:
-			print(bcolorsObject.FAIL + 'Error: File %s does not exist' % (str(sourceFile)) + bcolorsObject.ENDC, file = stderr)
-			continue
-		
-		#Compiling options
-		if options.compile or not options.noCompile:
-			executable = compileSource(sourceFile, options.verbose, options.optimize)
-		
-		if executable == '':
-			continue
-
-		if options.debug: #Debug
-			subprocess.call(['echo', '-ne', bcolorsObject.DEBUG])
-			subprocess.call(['gdb', '-q', executable])
-			subprocess.call(['echo', '-ne', bcolorsObject.ENDC])
-		elif options.test: #Test
-			for i in range(options.testingTimes): #Run the program testingTimes
-				print(bcolorsObject.DEBUG + 'Testing %s: %s times' % (str(executable), str(options.testingTimes - i)) + bcolorsObject.ENDC)
-				subprocess.call(['./' + executable]); 
-		elif options.evaluate: #Evaluate
-			evaluate(sourceFile, options.workingDirectory, options.evaluationTime, options.verbose, 
-					 options.ioiMode, options.totalMemory, options.noOuts, options.multipleSolutions, options.alternateValues)
+	checkEvalType(options, args)
 
